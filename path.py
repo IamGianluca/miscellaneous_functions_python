fc4bbf662fb0ab02780a02b8e792c06d3fae13ab
Gianluca Rossi <grossi@quantcast.com>
2016-01-12 17:27:30 +0000

    add function to print out the paths files within a directory

---
+++ path.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+
+def print_directory_contents(path):
+    """
+    This function takes the name of a directory and prints out the paths files within that
+    directory as well as any files contained in contained directories.
+
+    This function is similar to os.walk. I've built it just for learning purposes.
+    """
+    for child in os.listdir(path):
+        childPath = os.path.join(path, child)
+        if os.path.isdir(childPath):
+            print_directory_contents(childPath)
+        else:
+            print(childPath)
+
+
+if __name__ == "__main__":
+
+    # If the end user doesn't provide a specific path as an argument when calling the program, use the home directory
+    if len(sys.argv) == 1:
+        home = os.path.expanduser("~")
+    else:
+        home = str(sys.argv[1])
+    
+    print_directory_contents(home)
